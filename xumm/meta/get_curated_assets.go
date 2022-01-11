7d4e3972cda8d85c8535939d50c7ea8fd9eac65e
Joshua Long <joshua.antonio.long@gmail.com>
2022-01-11 22:20:01 +0000

    godocs function descriptions for Meta package

--- xumm/meta/get_curated_assets.go
+++ xumm/meta/get_curated_assets.go
@@ -13,6 +13,7 @@ const (
 	CURATEDASSETSENDPOINT = "/platform/curated-assets"
 )
 
+// GetCuratedAssets Gets curated assets from the XUMM API. This API contains the same issuers and assets available to users in XUMM when they press the "Add asset" button on the home screen.
 func (m *Meta) GetCuratedAssets() (*models.CuratedAssetsResponse, error) {
 	req, err := http.NewRequest(http.MethodGet, m.Cfg.BaseURL+CURATEDASSETSENDPOINT, nil)
 	req.Header = m.Cfg.GetHeaders()

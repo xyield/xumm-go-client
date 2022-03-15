062aec8a83c796f60141121f409ac4c984ce7226
bmurphy333 <brigidmurphy3@hotmail.co.uk>
2022-03-15 09:42:51 +0000

    added extra test cases

--- xumm/payload/payload_integration_test.go
+++ xumm/payload/payload_integration_test.go
@@ -1,10 +1,10 @@
+//go:build integration
 // +build integration
 
 package payload
 
 import (
 	"log"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,8 +14,119 @@ import (
 	xummdevice "github.com/xyield/xumm-user-device"
 )
 
-func TestPostPayloadIntegration(t *testing.T) {
-	xd := xummdevice.NewUserDevice(os.Getenv("XUMM_USER_DEVICE_ACCESS_TOKEN"), os.Getenv("XUMM_USER_DEVICE_UID"))
+// func TestPostPayloadIntegration_SignRequest(t *testing.T) {
+// 	xd := xummdevice.NewUserDevice(os.Getenv("XUMM_USER_DEVICE_ACCESS_TOKEN"), os.Getenv("XUMM_USER_DEVICE_UID"))
+// 	cfg, _ := xumm.NewConfig()
+
+// 	p := &Payload{
+// 		Cfg: cfg,
+// 	}
+
+// 	cp, err := p.PostPayload(models.XummPostPayload{
+// 		TxJson: anyjson.AnyJson{
+// 			"TransactionType": "Payment",
+// 			"Account":         "rQNrSWi3t6ojNFof8gE3Wq8Pwz88QUr6Hx",
+// 			"Amount":          "1",
+// 			"Destination":     "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
+// 			"Fee":             "12",
+// 			// Sequence: account_data.Sequence
+// 		},
+// 	})
+// 	if err != nil {
+// 		log.Println("Error creating payload", err)
+// 	}
+// 	err = xd.OpenPayload(cp.UUID)
+// 	if err != nil {
+// 		log.Println("Error opening payload:", err)
+// 	}
+// 	payload, err := p.GetPayloadByUUID(cp.UUID)
+// 	if err != nil {
+// 		log.Println("Error fetching payload", err)
+// 	}
+// 	assert.Equal(t, true, payload.Meta.AppOpened)
+
+// 	err = xd.SignRequest(cp.UUID, "Payment")
+// 	assert.NoError(t, err)
+
+// 	payload, err = p.GetPayloadByUUID(cp.UUID)
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, true, payload.Meta.Signed)
+// 	assert.Equal(t, true, payload.Meta.Resolved)
+// 	assert.Equal(t, true, payload.Meta.OpenedByDeeplink)
+// }
+
+// func TestPostPayloadIntegration_RejectRequest(t *testing.T) {
+// 	xd := xummdevice.NewUserDevice(os.Getenv("XUMM_USER_DEVICE_ACCESS_TOKEN"), os.Getenv("XUMM_USER_DEVICE_UID"))
+// 	cfg, _ := xumm.NewConfig()
+
+// 	p := &Payload{
+// 		Cfg: cfg,
+// 	}
+
+// 	cp, err := p.PostPayload(models.XummPostPayload{
+// 		TxJson: anyjson.AnyJson{
+// 			"TransactionType": "Payment",
+// 			"Account":         "rQNrSWi3t6ojNFof8gE3Wq8Pwz88QUr6Hx",
+// 			"Amount":          "1",
+// 			"Destination":     "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
+// 			"Fee":             "12",
+// 			// Sequence: account_data.Sequence
+// 		},
+// 	})
+// 	if err != nil {
+// 		log.Println("Error creating payload", err)
+// 	}
+// 	// utils.PrettyPrintJson(cp)
+// 	err = xd.OpenPayload(cp.UUID)
+// 	if err != nil {
+// 		log.Println("Error opening payload:", err)
+// 	}
+// 	payload, err := p.GetPayloadByUUID(cp.UUID)
+// 	if err != nil {
+// 		log.Println("Error fetching payload", err)
+// 	}
+// 	assert.Equal(t, true, payload.Meta.AppOpened)
+
+// 	err = xd.RejectRequest(cp.UUID)
+// 	assert.NoError(t, err)
+// 	payload, err = p.GetPayloadByUUID(cp.UUID)
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, false, payload.Meta.Signed)
+// 	assert.Equal(t, true, payload.Meta.Resolved)
+// }
+
+// func TestPostPayloadIntegration_CancelRequest(t *testing.T) {
+// 	// xd := xummdevice.NewUserDevice(os.Getenv("XUMM_USER_DEVICE_ACCESS_TOKEN"), os.Getenv("XUMM_USER_DEVICE_UID"))
+// 	cfg, _ := xumm.NewConfig()
+
+// 	p := &Payload{
+// 		Cfg: cfg,
+// 	}
+
+// 	cp, err := p.PostPayload(models.XummPostPayload{
+// 		TxJson: anyjson.AnyJson{
+// 			"TransactionType": "Payment",
+// 			"Account":         "rQNrSWi3t6ojNFof8gE3Wq8Pwz88QUr6Hx",
+// 			"Amount":          "1",
+// 			"Destination":     "rwietsevLFg8XSmG3bEZzFein1g8RBqWDZ",
+// 			"Fee":             "12",
+// 			// Sequence: account_data.Sequence
+// 		},
+// 	})
+// 	if err != nil {
+// 		log.Println("Error creating payload", err)
+// 	}
+// 	payload, err := p.GetPayloadByUUID(cp.UUID)
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, false, payload.Meta.Signed)
+// 	assert.Equal(t, false, payload.Meta.OpenedByDeeplink)
+// 	assert.Equal(t, false, payload.Meta.Resolved)
+// 	assert.Equal(t, true, payload.Meta.Cancelled)
+// 	assert.Equal(t, true, payload.Meta.Expired)
+// }
+
+func TestPostPayloadIntegration_SignRequestNotFound(t *testing.T) {
+	xd := xummdevice.NewUserDevice("548F5765-0FFF-49E5-AFBC-CE37517CA22E", "621928FBA984E")
 	cfg, _ := xumm.NewConfig()
 
 	p := &Payload{
@@ -35,7 +146,6 @@ func TestPostPayloadIntegration(t *testing.T) {
 	if err != nil {
 		log.Println("Error creating payload", err)
 	}
-	// utils.PrettyPrintJson(cp)
 	err = xd.OpenPayload(cp.UUID)
 	if err != nil {
 		log.Println("Error opening payload:", err)
@@ -45,11 +155,12 @@ func TestPostPayloadIntegration(t *testing.T) {
 		log.Println("Error fetching payload", err)
 	}
 	assert.Equal(t, true, payload.Meta.AppOpened)
-
 	err = xd.SignRequest(cp.UUID, "Payment")
-	assert.NoError(t, err)
-	payload, err = p.GetPayloadByUUID(cp.UUID)
-	assert.NoError(t, err)
-	assert.Equal(t, true, payload.Meta.Signed)
-	assert.Equal(t, true, payload.Meta.Resolved)
+	// uuid exists - or endpoint fails (it';s in the url)
+	// BUT there is no request to sign?
+	assert.Nil(t, err)
+
+	// payload, err := p.GetPayloadByUUID(cp.UUID)
+	// assert.Error(t, err)
+	// assert.Nil(t, payload)
 }
